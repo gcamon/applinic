eb952cc03ac6ac3ccd15f60274465876957fb2ce
gcamon <ede.obinna27@gmail.com>
2018-08-27 13:09:28 +0100

    voice call fix

--- route.js
+++ route.js
@@ -7331,8 +7331,8 @@ router.get("/user/doctor/initial-complaint",function(req,res){
 }) 
 
 router.get("/twiliovoicemsg",function(req,res){
-  var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n<Response>\n<Say>Thanks for your text, we\'ll be in touch.</Say>\n</Response>'
-  res.send(twiml, {'Content-Type':'text/xml'}, 200)
+  //var twiml = '<?xml version="1.0" encoding="UTF-8" ?>\n<Response>\n<Say>Thanks for your text, we\'ll be in touch.</Say>\n</Response>'
+  res.sendFile(path.join(__dirname + '/twiml.xml'))
 })
 
 
