57beba7f2a4a075e22421f084348f827703f06f0
gcamon <ede.obinna27@gmail.com>
2018-10-08 19:53:48 +0100

    changes made to video call

--- public/js/rtcClient.js
+++ public/js/rtcClient.js
@@ -85,7 +85,7 @@ var PeerManager = (function (name) {
   //if peer does not exist yet, this function will create peer below otherwise peer will be retreived fron 'peerDatabase' where existin
   //peer are kept. The remark where this happened in "jj".
   function addPeer(remoteId,name,id) {
-    var peer = new Peer(config.peerConnectionConfig, config.peerConnectionConstraints, name, id);
+    var peer = new Peer(config.peerConnectionConfig, config.peerConnectionConstraints, name);
     console.log("checking out peer object")
     peer.pc.onicecandidate = function(event) {
       if (event.candidate) {
@@ -231,11 +231,11 @@ var PeerManager = (function (name) {
 
       if(peerDatabase[remoteId]) {
         peerDatabase[remoteId].captionElement.innerHTML = name;
-        peerDatabase[remoteId].videoDiv.id = remoteId;
+        //peerDatabase[remoteId].videoDiv.id = remoteId;
       }
      
       //peer = peerDatabase[remoteId] || addPeer(remoteId,name); //'jj'
-      peer = peerDatabase[remoteId] || addPeer(remoteId,name,remoteId);
+      peer = peerDatabase[remoteId] || addPeer(remoteId,name);
       
       send('init', remoteId, null,name);
     },
@@ -265,7 +265,7 @@ var PeerManager = (function (name) {
   
 });
 
-var Peer = function (pcConfig, pcConstraints,name,id) {
+var Peer = function (pcConfig, pcConstraints,name) {
   //this.name = name //refers to the remote user name
   this.pc = new RTCPeerConnection(pcConfig, pcConstraints);
   this.remoteVideoEl = document.createElement('video');
@@ -276,7 +276,6 @@ var Peer = function (pcConfig, pcConstraints,name,id) {
   this.remoteVideoEl.style.width = "auto";
   this.remoteVideoEl.style.height = "250px";
   this.remoteVideoEl.style.width = "auto";
-  this.videoDiv.id = id;
   
   this.remoteVideoEl.controls = true;
   this.remoteVideoEl.autoplay = true;
