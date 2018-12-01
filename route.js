262b9ee77aeabe6bdb4affa848c1c805d7fc9a6a
gcamon <ede.obinna27@gmail.com>
2018-12-01 22:06:58 +0100

    twilio voip

--- route.js
+++ route.js
@@ -8060,7 +8060,7 @@ router.get("/user/doctor/initial-complaint",function(req,res){
   }
 });
 
-router.get("/twiliovoicemsg",function(req,res){
+router.post("/twiliovoicemsg",function(req,res){
   console.log(req.query)
   var twiml = 
   '<?xml version="1.0" encoding="UTF-8" ?><Response><Say>Your applinic.com verification code is obinna again 3455, thank you.</Say></Response>';
