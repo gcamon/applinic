2d7ed732642b3365b1c8fbed9b14daa496eb30c4
gcamon <ede.obinna27@gmail.com>
2019-01-23 20:17:53 +0100

    general fixes

--- socket.js
+++ socket.js
@@ -652,7 +652,7 @@ module.exports = function(model,io,streams,sms) {
     	sms.calls 
         .create({
           url: "https://applinic.com/inviteonlinecall?receiver=" +
-           data.receiver_name + "&&sender=" + data.sender + "&&type" + data.type,
+           data.receiver_name + "&&sender=" + data.sender + "&&type=" + data.type,
            to: "+2348064245256", //data.receiver_phone,//"+2348064245256",
            from: '+16467985692',
         })
