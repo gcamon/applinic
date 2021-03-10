7c8df0a48e6eba68ac02790f2be0c8391ba7c770
gcamon <ede.obinna27@gmail.com>
2021-03-10 18:01:06 +0100

    robot.txt fix commit

--- route.js
+++ route.js
@@ -14202,9 +14202,9 @@ router.get("/sitemap.xml",function(req,res){
 });
 
 router.get("/robots.txt",function(req,res){
-  //res.sendFile(path.join(__dirname, 'robots.txt'));
-  res.type('text/plain');
-  res.send("User-agent: *\nDisallow: /download/\nDisallow: /pdf/\nDisallow: /api/\nDisallow: /report/\nDisallow: /chat-files/\nAllow: /sitemap.xml");
+  res.sendFile(path.join(__dirname, 'robots.txt'));
+  //res.type('text/plain');
+  //res.send("User-agent: *\nDisallow: /download/\nDisallow: /pdf/\nDisallow: /api/\nDisallow: /report/\nDisallow: /chat-files/\nAllow: /sitemap.xml");
 });
 
 
