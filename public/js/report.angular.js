235504b5bdfad64abe366783ca68291ef8393b54
unknown <gcammon>
2023-09-07 06:19:51 +0100

    fiox prefix issue

--- public/js/report.angular.js
+++ public/js/report.angular.js
@@ -41,6 +41,7 @@ app.controller("ultraSoundReportCtrl",["$scope","$http","localManager","$rootSco
       {params:{centerEmail: studyDetails.centerEmail,ref_uid: studyDetails.studyIUID}})
     .success(function(responseData){
       var data = responseData.center || {};
+      alert(localManager.getValue("reportEntry"))
       $scope.ultraRefData = (localManager.getValue("reportEntry")) 
       ? (localManager.getValue("reportEntry")) : (responseData.ris || {radiology:{}});
       //localManager.removeItem("reportEntry");
